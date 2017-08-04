b0807e6d0b4a13f798d12036df585fda00ef603b
Jusmar <jusmar3105@gmail.com>
2017-08-04 08:40:04 -0500

    Home_v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MdInputModule } from '@angular/material';
-
+import { ChartsModule } from 'ng2-charts';
 import {APP_ROUTING} from './app.routes';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MdInputModule,
+    ChartsModule,
     APP_ROUTING
   ],
   providers: [],
